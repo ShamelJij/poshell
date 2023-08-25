0d38955281a374fe4be445bd081ed7b348cef36c
shameljij <shamel.jij@gmail.com>
2023-08-25 11:01:51 +0200

    added navbar

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,8 +16,43 @@ Function checknet {
 }
 
 Function jsdir {
+    $something = "this is a test";
+    $ranUserName = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.login.username;
+    $ranFirstName = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.name.first;
+    $ranCountry = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.location.country;
+    $ranEmail = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.email;
     $randomPic = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.picture.thumbnail;
-    $randomJSDirectory = (Invoke-RestMethod -Uri "https://randomuser.me/api/?gender=female").results.name.first;
+    $page = '<!doctype html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>$ranUserName</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light">
+      <div><img class="rounded-circle" width="50" height="50" src="' + $randomPic + '"/></div>
+      <div>' + $ranEmail + '</div>
+      <div>' + $ranUserName + '</div>
+      <div>' + $ranFirstName + '</div>
+      <div>' + $ranCountry + '</div>
+    </nav>
+    <h1 class="text-center">' + $ranUserName + '</h1>
+
+    <!-- Optional JavaScript -->
+    <script src="app.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>';
+    $randomJSDirectory = $ranUserName;
     $directoryPath = "$env:myStart\randomJS";
 
 
@@ -29,30 +64,7 @@ Function jsdir {
         }
     New-Item -Path $directoryPath -Name  $randomJSDirectory -ItemType Directory;
     New-Item -Path ($directoryPath + "\" + $randomJSDirectory) -Name "index.html" -ItemType File;
-    set-content -Path ($directoryPath + "\" + $randomJSDirectory + "/index.html") -Value '<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  <div class="col text-center"><img class="rounded-circle" src="https://xsgames.co/randomusers/avatar.php?g=female"/></div>
-    <h1 class="text-center">Hello, world!</h1>
-
-    <!-- Optional JavaScript -->
-    <script src="app.js"></script>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>'
+    set-content -Path ($directoryPath + "\" + $randomJSDirectory + "/index.html") -Value $page
     New-Item -Path ($directoryPath + "\" + $randomJSDirectory) -Name "app.js" -ItemType File;
     New-Item -Path ($directoryPath + "\" + $randomJSDirectory) -Name "style.css" -ItemType File;
     cd ($directoryPath + "\" + $randomJSDirectory)
